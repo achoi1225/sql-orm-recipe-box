e36c3f5fc8bb3e20129eea3a5c3b343bdb465411
Jessica Abels <jessabels92@gmail.com>
2020-09-17 15:07:24 -0700

    Finish phase 6

---
+++ migrations/20200917213319-create-instruction.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Instructions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      specification: {
+        type: Sequelize.TEXT,
+      },
+      listOrder: {
+        type: Sequelize.INTEGER,
+      },
+      recipeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "Recipes" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Instructions");
+  },
+};
